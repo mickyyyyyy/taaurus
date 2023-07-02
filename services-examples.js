69b59cdde73271459281e87e48964a94c6b2e149
Michael Hammond <michael.hammond@uqconnect.edu.au>
2023-07-02 22:43:48 +1000

    Add files via upload
    Updated the services examples gallery to cycle.

--- services-examples.js
+++ services-examples.js
@@ -1,19 +1,30 @@
-let scrollContainer = document.querySelector(".example-wrapper");
-let backBtn = document.getElementById("back-button");
-let nextBtn = document.getElementById("forward-button");
-const services = document.getElementById("services-examples");
-//const rect = services.getBoundingClientRect();
-
+// Creating a delay function
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Scrolling variables
+let scrollContainer = document.querySelector(".example-wrapper");
+let backBtn = document.getElementById("back-button");
+let nextBtn = document.getElementById("forward-button");
+const services = document.getElementById("services-examples");
+let examplesDiv = document.getElementById('services-examples');
+let examples = examplesDiv.getElementsByClassName("media");
+
+// Start in the middle
+scrollContainer.scrollLeft += services.offsetWidth;
+
 nextBtn.addEventListener("click", () => {
-  scrollContainer.style.scrollBehavior = 'smooth';
-  scrollContainer.scrollLeft += services.offsetWidth;// rect.width;
+  // Add the leftmost example to the very right
+  const head = examples[0];
+  const tail = examples[examples.length-1];
+  tail.after(head);
 });
 
+// Go to the previous service example
 backBtn.addEventListener("click", () => {
-  scrollContainer.style.scrollBehavior = 'smooth';
-  scrollContainer.scrollLeft -= services.offsetWidth;//rect.width;
-})
+  // Add the rightmost example to the very left
+  const head = examples[0];
+  const tail = examples[examples.length-1];
+  head.before(tail);
+});
